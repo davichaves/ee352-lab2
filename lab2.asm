383dfa77a1033ef176e9fea69f626bd9005823bd
Davi Chaves <dchaves@usc.edu>
2015-09-25 00:29:12 -0700

    lab 2

---
+++ lab2.asm
@@ -0,0 +1,8 @@
+# Title: 	Lab1 assignment
+# Author: 	Davi Rodrigues Chaves
+# Due Date:	Oct. 2nd, 2015
+# Date:		Sep. 11th, 2015
+      .data
+fibs: .word	0 : 10        # "array" of 10 words to contain the values
+size: .word	10             # size of "array" 
+
